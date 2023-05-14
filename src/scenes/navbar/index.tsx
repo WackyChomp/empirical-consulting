1ef8875529074d4a7964303ab261ebdf9e0fc26a
Wilson Chan <wilsonchan54@gmail.com>
2023-05-14 10:45:30 -0400

    Created useMediaQuery.ts with code snippet / added media query for nav items in navbar component

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from '@/assets/react.svg'
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
 	selectedPage: SelectedPage;
@@ -11,6 +12,9 @@ type Props = {
 
 const Navbar = ( { selectedPage, setSelectedPage }: Props) => {
 	const flexBetween = 'flex items-center justify-between'
+	const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");		// hook
+	const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+
   return (
     <nav>
 			{/* outer navbar */}
@@ -21,7 +25,7 @@ const Navbar = ( { selectedPage, setSelectedPage }: Props) => {
 						{/* left */}
 						<img src={Logo} alt="logo image here" />
 						{/* right */}
-						<div className={`${flexBetween} w-full`}>
+						{isAboveMediumScreens ? (<div className={`${flexBetween} w-full`}>
 							<div className={`${flexBetween} gap-8 text-sm`}>
 								<Link page='Home' selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
 								<Link page='Perks' selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
@@ -33,6 +37,15 @@ const Navbar = ( { selectedPage, setSelectedPage }: Props) => {
 								<button>Join the Agency</button>
 							</div>
 						</div>
+						)
+							:(
+								<button 
+									className="rounded-full bg-secondary-500 p-2"
+									onClick={() =>{setIsMenuToggled(!isMenuToggled)}}
+								>
+									<Bars3Icon className="h-6 w-6 text-purple-800" />
+								</button>
+							)}						
 					</div>
 				</div>
 			</div>
