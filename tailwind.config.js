127407a7789b8a2012e8f21da65992b37616293b
Wilson Chan <wilsonchan54@gmail.com>
2023-05-26 22:05:01 -0400

    Generated colors with tailwind shades extension in index.css / changed colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        "gray-20": "#F8F4EB",
+        "gray-20": "#ebf4f8",
         "gray-50": "#EFE6E6",
         "gray-100": "#DFCCCC",
-        "gray-500": "#5E0000",
-        "primary-100": "#FFE1E0",
-        "primary-300": "#FFA6A3",
-        "primary-500": "#FF6B66",
+        "gray-500": "#0a2855",      /* dark blue color 800 */
+        "primary-100": "#d1e0f6",   /* from #1964d4 */
+        "primary-300": "#75a2e5",   /* from #1964d4 */
+        "primary-500": "#1964d4",   /* from #1964d4 */
         "secondary-400": "#FFCD5B",
         "secondary-500": "#FFC132",
       },
